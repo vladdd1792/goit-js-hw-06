4738792fa34679b289a03a6f2d38a3ff5cc57292
vladdd1792 <107995468+vladdd1792@users.noreply.github.com>
2022-11-15 12:56:24 +0200

    Delete task-07.js

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-let controlFont = document.getElementById("font-size-control");
-
-let text = document.getElementById("text");
-
-controlFont.oninput = function() {
-  text.style.fontSize = controlFont.value + "px";
-};
